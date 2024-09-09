98bc4dac0893f11d03ba8f5732084f0b6dd94ae9
razorext2 <razorext2@gmail.com>
2024-09-09 15:07:22 +0700

    update route: dashboard, api images, pegawai dll

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// breeze for regist, verif, login and logout
+Route::get('/', function () {
+    return view('home');
+})->name('landing.page');
+
+
+Route::get('/dashboard', [\App\Http\Controllers\AdminController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+// end breeze
+
+// api for get pegawai data
 Route::get('/api/getPegawai', [\App\Http\Controllers\PegawaiController::class, 'getPegawai']);
 Route::get('/api/pegawai-images/{id}', [\App\Http\Controllers\PegawaiController::class, 'getPegawaiImages']);
 Route::post('/api/saveImage', [\App\Http\Controllers\PegawaiController::class, 'storeImage']);
-
 Route::get('/api/getPegawaiData/{label}', [\App\Http\Controllers\PegawaiController::class, 'getPegawaiDataByLabel']);
 
 // absen
@@ -17,6 +36,3 @@ Route::post('/store-attendance-out', [\App\Http\Controllers\AttendanceOutControl
 // daftar
 Route::get('/photo-regist', [\App\Http\Controllers\PegawaiController::class, 'photoRegist'])->name('photo.regist');
 Route::post('/photo-regist-process', [\App\Http\Controllers\PegawaiController::class, 'photoRegistProcess'])->name('photo.registProcess');
-
-// admin
-Route::get('/dashboard', [\App\Http\Controllers\AdminController::class, 'index'])->name('dashboard');
