d59c0e03e28e889064398d29c8d6cdedac87ccfc
razorext2 <razorext2@gmail.com>
2024-09-09 15:14:12 +0700

    working absence section. it will show the data of the attendance of the day

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -92,19 +92,44 @@
     </div>
 
     <div class="grid grid-cols-1 w-full">
-        <div class="flex flex-col">
-            <span class="bg-gray-100 ring-1 ring-gray-200 p-4 rounded-lg">
-                <time class="text-md font-semibold text-gray-900">January 13th, 2022</time>
+        <div class="flex flex-col bg-white shadow-sm ring-1 ring-gray-200 p-4 rounded-lg">
+            <span>
+                <time class="text-md font-semibold text-gray-900">Absen Masuk, {{ \Carbon\Carbon::today()->locale('id')->isoFormat('D MMMM YYYY') }}</time>
             </span>
-            <ul class="max-h-96 overflow-y-auto text-gray-700" aria-labelledby="dropdownUsersButton">
-                <?php for ($i = 0; $i <= 10; $i++) { ?>
-                    <li>
-                        <a href="#" class="flex items-center p-4 bg-[#7678ed] text-white hover:bg-[#5d5ece] my-4 rounded-lg">
-                            <img class="w-6 h-6 me-2 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Jese image">
-                            Jese Leos, melakukan absensi pada pukul 08.00 WIB
-                        </a>
-                    </li>
-                <?php } ?>
+            <ul class="max-h-48 overflow-y-auto text-gray-700" aria-labelledby="dropdownUsersButton">
+
+                @foreach ( $attendance_today as $at )
+
+                <li>
+                    <a href="#" class="flex items-center p-2 bg-[#7678ed] text-white hover:bg-[#5d5ece] my-2 rounded-lg">
+                        <img class="w-6 h-6 me-2 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Jese image">
+                        {{ $at->pegawai->nick_name }}, melakukan absensi <span class="text-white bg-green-400 px-2 mx-1 rounded-lg font-semibold"> masuk </span> pada pukul {{ \Carbon\Carbon::parse($at->jam_masuk)->format('H:i') }}
+                    </a>
+                </li>
+
+                @endforeach
+
+            </ul>
+
+        </div>
+
+        <div class="flex flex-col mt-3 bg-white shadow-sm ring-1 ring-gray-200 p-4 rounded-lg">
+            <span>
+                <time class="text-md font-semibold text-gray-900">Absen Keluar, {{ \Carbon\Carbon::today()->locale('id')->isoFormat('D MMMM YYYY') }}</time>
+            </span>
+            <ul class="max-h-48 overflow-y-auto text-gray-700" aria-labelledby="dropdownUsersButton">
+
+                @foreach ( $attendance_out_today as $at )
+
+                <li>
+                    <a href="#" class="flex items-center p-2 bg-[#7678ed] text-white hover:bg-[#5d5ece] my-2 rounded-lg">
+                        <img class="w-6 h-6 me-2 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Jese image">
+                        {{ $at->pegawai->nick_name }}, melakukan absensi <span class="text-white bg-red-400 px-2 mx-1 rounded-lg font-semibold"> keluar </span> pada pukul {{ \Carbon\Carbon::parse($at->jam_keluar)->format('H:i') }}
+                    </a>
+                </li>
+
+                @endforeach
+
             </ul>
 
         </div>
@@ -115,83 +140,23 @@
 
 <div class="grid grid-cols-1 gap-6">
     <div class="flex items-center justify-center rounded-lg bg-gray-50 h-auto p-4 ring-1 ring-gray-200 shadow-sm">
-        <table id="filter-table">
-            <thead>
-                <tr>
-                    <th>
-                        <span class="flex items-center">
-                            Kode Pegawai
-                            <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                            </svg>
-                        </span>
-                    </th>
-                    <th>
-                        <span class="flex items-center">
-                            NIK
-                            <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                            </svg>
-                        </span>
-                    </th>
-                    <th>
-                        <span class="flex items-center">
-                            Nama Pegawai
-                            <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                            </svg>
-                        </span>
-                    </th>
-                    <th>
-                        <span class="flex items-center">
-                            No Telepon
-                            <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                            </svg>
-                        </span>
-                    </th>
-                    <th>
-                        <span class="flex items-center">
-                            Jabatan
-                            <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                            </svg>
-                        </span>
-                    </th>
-                    <th>
-                        <span class="flex items-center">
-                            Jam Masuk
-                            <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                            </svg>
-                        </span>
-                    </th>
+        <!-- <table>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
 
-                    <th>
-                        <span class="flex items-center">
-                            Jam
-                            <svg class="w-4 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8 15 4 4 4-4m0-6-4-4-4 4" />
-                            </svg>
-                        </span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php for ($i = 0; $i <= 20; $i++) { ?>
-                    <tr>
-                        <td class="font-medium text-gray-900 whitespace-nowrap">Apple iMac</td>
-                        <td>Computers</td>
-                        <td>Apple</td>
-                        <td>$1,299</td>
-                        <td>50</td>
-                        <td>200</td>
-                        <td>In Stock</td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table> -->
     </div>
 
 </div>
