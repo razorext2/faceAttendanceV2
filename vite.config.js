5d99abb8431a9246fda9a59e3b0aa89747224c82
razorext2 <razorext2@gmail.com>
2024-09-09 16:09:12 +0700

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [laravel(["resources/css/app.css", "resources/js/app.js"])],
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
 });
