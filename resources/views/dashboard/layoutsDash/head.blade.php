ef89f82fa98b7da8f1b808f3c7aa4149f7f475f1
razorext2 <razorext2@gmail.com>
2024-09-09 14:08:31 +0700

    add chart.js and simpletables.js for dashboard

--- resources/views/dashboard/layoutsDash/head.blade.php
+++ resources/views/dashboard/layoutsDash/head.blade.php
@@ -15,7 +15,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
 
 <!-- Vendor CSS Files -->
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/chart.js', 'resources/js/simpleTables.js'])
 <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet" />
