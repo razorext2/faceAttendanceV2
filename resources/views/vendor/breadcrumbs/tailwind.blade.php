b7c373ea0f9729c344e685fa4ce974e3ce809df3
razorext2 <razorext2@gmail.com>
2024-09-10 14:19:33 +0700

    adjust gap

--- resources/views/vendor/breadcrumbs/tailwind.blade.php
+++ resources/views/vendor/breadcrumbs/tailwind.blade.php
@@ -1,5 +1,5 @@
 @unless ($breadcrumbs->isEmpty())
-<nav class="flex mb-4" aria-label="Breadcrumb">
+<nav class="flex mb-2" aria-label="Breadcrumb">
     <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
         @foreach ($breadcrumbs as $breadcrumb)
 
