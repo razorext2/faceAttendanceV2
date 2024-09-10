459419c00195c140f243852cf9c0fd2fa15e2ab4
razorext2 <razorext2@gmail.com>
2024-09-10 11:37:45 +0700

    make fully working email verification function

--- resources/views/dashboard/profile/partials/update-profile-information-form.blade.php
+++ resources/views/dashboard/profile/partials/update-profile-information-form.blade.php
@@ -23,41 +23,36 @@
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
+        <div class="flex items-center space-x-2">
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
+            <div class="flex-grow">
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" name="email" type="text" class="mt-1 block w-full disabled:bg-gray-300" :value="old('email', $user->email)" disabled autofocus autocomplete="email" />
+                <x-input-error class="mt-2" :messages="$errors->get('email')" />
+            </div>
+            @if(!is_null($user->email_verified_at))
+            <span class="text-green-800 mt-6 p-2 bg-green-200 text-center rounded-lg ring-1 ring-green-800 shadow-sm">Verified</span>
+            @else
+            <button form="send-verification" class="text-red-800 mt-6 p-2 bg-red-200 text-center rounded-lg ring-1 ring-red-800 shadow-sm">Not Verified</button>
             @endif
         </div>
+
+        @if (session('status') == 'verification-link-sent')
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+        </div>
+        @endif
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+            <p
+                x-data="{ show: true }"
+                x-show="show"
+                x-transition
+                x-init="setTimeout(() => show = false, 2000)"
+                class="text-sm text-gray-600">{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
