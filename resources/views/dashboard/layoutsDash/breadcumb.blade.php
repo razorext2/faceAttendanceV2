1182910c67e4df75c7037c6923841dae237daaf1
razorext2 <razorext2@gmail.com>
2024-09-11 10:17:02 +0700

    ketinggalan juga

--- resources/views/dashboard/layoutsDash/breadcumb.blade.php
+++ resources/views/dashboard/layoutsDash/breadcumb.blade.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-1 gap-4">
-    {{ Breadcrumbs::render() }}
+    {{ Diglactic\Breadcrumbs\Breadcrumbs::render() }}
     @if (Route::currentRouteName() == 'dashboard')
     <h2 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl">Dashboard</h2>
     @elseif (Route::currentRouteName() == 'dashboard.pegawai')
@@ -15,6 +15,5 @@
         Profile -
         <span class="text-lg font-bold md:text-xl" style="font-size: 24px !important;">{{ Auth::user()->name }}</span>
     </h2>
-
     @endif
 </div>
