284da70e3fc937a869c4e181e2c58272274923b5
razorext2 <razorext2@gmail.com>
2024-11-09 15:11:29 +0700

    fix foto geprek lagi

--- resources/views/dashboard/pegawai/edit.blade.php
+++ resources/views/dashboard/pegawai/edit.blade.php
@@ -269,33 +269,35 @@
 				startCamera();
 				setTimeout(() => {
 					displayTimer(3, () => {
-						const photo1 = capturePhoto(canvas, 1280, 720);
+						const photo1 = capturePhoto(canvas, video.videoWidth, video.videoHeight);
 						photo1Data.value = photo1;
 
 						// Menunggu 3 detik sebelum menangkap foto kedua
 						setTimeout(() => {
 							displayTimer(3, () => {
-								const photo2 = capturePhoto(canvass, 1280, 720);
+								const photo2 = capturePhoto(canvass, video.videoWidth, video
+									.videoHeight);
 								photo2Data.value = photo2;
 							});
 						}, 3000); // Jeda sebelum menangkap foto kedua
 					});
 				}, 3000); // Jeda sebelum menangkap foto pertama
 
-			} else {
-				// Jika kamera sudah dimulai, langsung menangkap foto
-				displayTimer(3, () => {
-					const photo1 = capturePhoto(canvas, 1280, 720);
-					photo1Data.value = photo1;
-
-					setTimeout(() => {
-						displayTimer(3, () => {
-							const photo2 = capturePhoto(canvass, 1280, 720);
-							photo2Data.value = photo2;
-						});
-					}, 3000); // Jeda sebelum menangkap foto kedua
-				});
 			}
+			// else {
+			// 	// Jika kamera sudah dimulai, langsung menangkap foto
+			// 	displayTimer(3, () => {
+			// 		const photo1 = capturePhoto(canvas, 1280, 720);
+			// 		photo1Data.value = photo1;
+
+			// 		setTimeout(() => {
+			// 			displayTimer(3, () => {
+			// 				const photo2 = capturePhoto(canvass, 1280, 720);
+			// 				photo2Data.value = photo2;
+			// 			});
+			// 		}, 3000); // Jeda sebelum menangkap foto kedua
+			// 	});
+			// }
 		}
 
 		// Event listener untuk tombol capture
