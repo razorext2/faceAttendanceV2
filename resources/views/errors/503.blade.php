f8decc04813d7428dbf1aabfe5170d67324b0789
razorext2 <razorext2@gmail.com>
2024-11-25 11:04:11 +0700

    menambah halaman error, menyesuaikan dengan template yang ada

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+
+		<title>Maintenance Mode</title>
+
+		<!-- Fonts -->
+		<link href="https://fonts.bunny.net" rel="preconnect">
+		<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+		<!-- Scripts -->
+		@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+		<script>
+			// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+			if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+					'(prefers-color-scheme: dark)').matches)) {
+				document.documentElement.classList.add('dark');
+			} else {
+				document.documentElement.classList.remove('dark')
+			}
+		</script>
+	</head>
+
+	<body class="dark:bg-gray-900 bg-white antialiased">
+		<div class="container mx-auto px-8">
+			<div class="grid h-screen md:grid-cols-2">
+				<!-- Left section (Image) -->
+				<div class="flex items-center justify-center">
+					<img class="w-64 md:w-full" src="{{ asset('assets/img/503.png') }}" alt="503 Image">
+				</div>
+
+				<!-- Right section (Text) -->
+				<div class="flex items-center justify-center">
+					<div class="mx-auto max-w-screen-xl lg:px-6 lg:py-16">
+						<div class="mx-auto max-w-screen-sm">
+							<span class="mb-4 text-8xl font-bold text-blue-500">503</span>
+							<p class="dark:text-white mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">
+								Sorry! we're currently on maintenance mode.
+							</p>
+							<p class="dark:text-gray-400 mb-4 text-lg font-light text-gray-500">
+								We'll be back soon as possible, grab your coffee while waiting for us to come back.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</body>
+
+</html>
