8b4ac5d0bf136a3fb609e99d0c5dc405283de102
razorext2 <razorext2@gmail.com>
2024-12-02 09:54:51 +0700

    test connect to main server using API

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PegawaiController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 
 Route::apiResource('/collectors', ApiCollectorController::class);
@@ -15,6 +16,16 @@ Route::patch('collectors/{collector}/confirm', [ApiCollectorController::class, '
 Route::patch('collectors/{collector}/deny', [ApiCollectorController::class, 'denyCollect']);
 
 Route::apiResource('/collector-photos', ApiPhotoCollectController::class);
+
+// api ke server utama
+Route::post('proxy/server/attendance', function (Request $request) {
+    // 
+    $response = Http::post('https://indodacin.nusa.net.id/web/finger/secureapi.php?tipe=insertAttendance', [
+        'kode_jari' => $request->input('kode_jari'),
+    ]);
+
+    return $response->json();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
