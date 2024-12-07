b2f3dd422cf9a8a3be43d80ddaf1dc62fa25aa80
razorext2 <razorext2@gmail.com>
2024-12-07 14:27:48 +0700

    adjust: ubah tombol dark dan light mode

--- resources/views/dashboard/layoutsDash/footer.blade.php
+++ resources/views/dashboard/layoutsDash/footer.blade.php
@@ -1,26 +1,11 @@
 <div>
-	<button class="h-10 w-10 rounded-md p-2.5 text-sm text-gray-200" id="theme-toggle-dark" type="button"
-		aria-label="Button Dark">
-		<svg class="h-5 w-5" id="theme-toggle-dark-icon" fill="currentColor" viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg">
-			<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-		</svg>
-	</button>
-
-	<button class="h-10 w-10 rounded-md p-2.5 text-sm text-gray-200" id="theme-toggle-light" type="button"
-		aria-label="Button Light">
-		<svg class="h-5 w-5" id="theme-toggle-light-icon" fill="currentColor" viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg">
-			<path
-				d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-				fill-rule="evenodd" clip-rule="evenodd"></path>
-		</svg>
-	</button>
+	<x-button-dark />
+	<x-button-light />
 </div>
 
-<footer class="dark:border-t-gray-700 border-t border-t-gray-100">
+<footer class="border-t border-t-gray-100 dark:border-t-gray-700">
 	<div class="w-full pt-2 md:flex md:justify-end">
-		<span class="dark:text-gray-300 text-sm text-gray-500">©
+		<span class="text-sm text-gray-500 dark:text-gray-300">©
 			@php
 				echo date('Y');
 			@endphp
