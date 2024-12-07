025606fed5d3c0463bde95c657e956bc6de7f85b
razorext2 <razorext2@gmail.com>
2024-12-07 14:35:27 +0700

    adjust: dropdown pada navbar

--- resources/views/dashboard/layoutsDash/navbar.blade.php
+++ resources/views/dashboard/layoutsDash/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="dark:bg-[#18181b] dark:border-[#232327] fixed top-0 z-50 w-full border-b border-gray-200 bg-white">
+<nav class="fixed top-0 z-50 w-full border-b border-gray-200 bg-white dark:border-[#232327] dark:bg-[#18181b]">
 	<div class="px-3 py-3 lg:px-5 lg:pl-3">
 		<div class="flex items-center justify-between">
 			<div class="flex items-center justify-start">
@@ -14,10 +14,10 @@
 						<button class="flex rounded-full text-sm" type="button" @click="profile = !profile">
 							<span class="sr-only">Open user menu</span>
 							<div class="me-2 hidden items-center space-x-6 rtl:space-x-reverse md:block">
-								<p class="dark:text-white text-right text-sm text-gray-800" role="none">
+								<p class="text-right text-sm text-gray-800 dark:text-white" role="none">
 									{{ Auth::user()->name }}
 								</p>
-								<p class="dark:text-white truncate text-right text-sm font-medium text-gray-800" role="none">
+								<p class="truncate text-right text-sm font-medium text-gray-800 dark:text-white" role="none">
 									{{ Auth::user()->email }}
 								</p>
 							</div>
@@ -27,29 +27,29 @@
 					</div>
 
 					<div
-						class="dark:bg-[#18181b] dark:divide-gray-500 absolute right-5 top-16 z-50 list-none rounded-lg bg-white text-base ring-1 ring-gray-700"
+						class="absolute right-5 top-14 z-50 list-none rounded-lg bg-white text-base ring-1 ring-gray-200 dark:divide-gray-500 dark:bg-[#18181b]"
 						style="display: none;" x-show="profile" x-transition:enter="transition ease-in duration-200"
 						x-transition:enter-start="transform opacity-0 -translate-y-5"
 						x-transition:leave="transition ease-out duration-200"
 						x-transition:leave-end="transform opacity-0  -translate-y-5">
-						<div class="dark:border-gray-700 block cursor-default border-b border-gray-200 px-4 py-3 md:hidden"
+						<div class="block cursor-default border-b border-gray-200 px-4 py-3 dark:border-gray-700 md:hidden"
 							role="none">
-							<p class="dark:text-white text-sm text-gray-800" role="none">
+							<p class="text-sm text-gray-800 dark:text-white" role="none">
 								{{ Auth::user()->name }}
 							</p>
-							<p class="dark:text-white truncate text-sm font-medium text-gray-800" role="none">
+							<p class="truncate text-sm font-medium text-gray-800 dark:text-white" role="none">
 								{{ Auth::user()->email }}
 							</p>
 						</div>
 						<ul class="p-2" role="none">
 							<li>
 								<a
-									class="dark:hover:bg-gray-700 dark:text-gray-300 dark:hover:text-white block rounded-md py-2 pl-3 pr-16 text-sm text-gray-800 hover:bg-gray-100"
+									class="block rounded-md py-2 pl-3 pr-16 text-sm text-gray-800 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white"
 									href="{{ route('landing.page') }}" role="menuitem" target="_blank">Home</a>
 							</li>
 							<li>
 								<x-dropdown-link
-									class="dark:hover:bg-gray-700 dark:text-gray-300 dark:hover:text-white rounded-md pl-3 pr-16 text-gray-800 hover:bg-gray-100"
+									class="rounded-md pl-3 pr-16 text-gray-800 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white"
 									:href="route('profile.edit')">
 									{{ __('Profile') }}
 								</x-dropdown-link>
@@ -59,7 +59,7 @@
 									@csrf
 
 									<x-dropdown-link
-										class="dark:hover:bg-gray-700 dark:text-red-500 dark:hover:text-red-600 rounded-md pl-3 pr-16 text-gray-800 hover:bg-gray-100"
+										class="rounded-md pl-3 pr-16 text-red-500 hover:bg-gray-100 hover:text-red-600 dark:hover:bg-gray-700"
 										:href="route('logout')"
 										onclick="event.preventDefault();
                             this.closest('form').submit();">
